d88b5c900262e9c5ab9a976788ba130edbf425d6
s.matthew.english <s.matthew.english@gmail.com>
2018-02-18 18:55:13 -0500

    basic add operator

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define MEMSIZE 10
-#define TERMINATE -9
+#define TERMINATE -99999
 
 /* Input/output operations: */
 #define READ 10       // Read a word from the terminal into a specific location in memory.
@@ -22,6 +22,15 @@
 #define BRANCHNEG 41  // Branch to a specific location in memory if the accumulator is negative. 
 #define BRANCHZERO 42 // Branch to a specific location in memory if the accumulator is zero.
 #define HALT 43       // Halt- i.e., the program has completed its task. 
+
+// 1007
+// 1008
+// 2007
+// 3008
+// 2109
+// 1109
+// 4300
+// -99999
 
 void printMemoryContents(int memory[]) {
   for(int i = 0; i < 10; i++) {
@@ -62,72 +71,48 @@ int getOperand(int aggregateValue) {
 }
 
 void executeByteCode(int memory[], int accumulator) {
-  int programCounter;
+  int programCounter = 0;
   int operationCode;
   int operand;
 
-  for(int i = 0; i < 10; i++) {
-    printf("%d",memory[i]);
+  while(programCounter < 10) {
 
-    programCounter = i;
-    operationCode = getOpCode(memory[i]);
-    operand = getOperand(memory[i]);
+    operationCode = getOpCode(memory[programCounter]);
+    //printf("operationCode: %d\n", operationCode);
+    operand = getOperand(memory[programCounter]);
+    //printf("operand: %d\n", operand);
 
-  switch(operationCode) {
-    case READ:
-
-      (*instructionCounter)++;
-      break;
-    case WRITE:
-
-      (*instructionCounter)++;
-      break;
-    case LOAD:
-
-      (*instructionCounter)++;
-      break;
-    case STORE:
-
-      (*instructionCounter)++;
-      break;
-    case ADD:
-      *accumulator = memory[programCounter] / *accumulator;
-      (*instructionCounter)++;
-      break;
-    case SUBTRACT:
-
-      (*instructionCounter)++;
-      break;
-    case DIVIDE:
-      *accumulator = memory[programCounter] / *accumulator;
-      (*instructionCounter)++;
-      break;
-    case MULTIPLY:
-
-      (*instructionCounter)++;
-      break;
-    case BRANCH:
-
-      (*instructionCounter)++;
-      break;
-    case BRANCHNEG:
-
-      (*instructionCounter)++;
-      break;
-    case BRANCHZERO:
-
-      (*instructionCounter)++;
-      break;
-    case HALT:
-
-      (*instructionCounter)++;
-      break;
-  }
-
+    switch(operationCode) {
+      case READ:
+        printf("READ: ");
+        scanf("%d", &memory[operand]);
+        programCounter++;
+        break;
+      case WRITE:
+        printf("WRITE: ");
+        printf("%d\n", memory[operand]);
+        programCounter++;
+        break;
+      case LOAD:
+        printf("load: %d\n", operand);
+        accumulator = memory[operand];
+        programCounter++;
+        break;
+    case STORE :
+        printf("store: %d\n", operand);
+        memory[operand] = accumulator;
+        programCounter++;
+        break;
+      case ADD:
+        accumulator = memory[operand] + accumulator;
+        programCounter++;
+        break;
+      case HALT:
+        programCounter = 10;
+        break;
+    }
   }
 }
-
-
 
 int main()
 {
@@ -136,8 +121,6 @@ int main()
   int accumulator;
 
   readInputFromConsole(memory);
-
-  printMemoryContents(memory);
 
   executeByteCode(memory, accumulator);
 
